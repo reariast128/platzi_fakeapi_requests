bbd919c0c8a6b270bbfc4682e00d070d498fb572
Ricardo Arias <reariast128@gmail.com>
2023-11-13 17:12:12 -0500

    Idk. jkashdflkajsdhflkasdjhfalksjdf.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import api_handle
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3, 4, 5]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Muy buenas tardes soy Creep1ng.</h1>
+"""
 
 def run():
     response = api_handle.get_categories()
